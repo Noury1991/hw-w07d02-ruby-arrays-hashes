5bfa40bacb4bd286a1023d80488225d6309dceb5
noura <no_-_no@hotmail.com>
2019-11-17 01:46:59 +0300

    Update solve

--- mbta.rb
+++ mbta.rb
@@ -1,39 +1,20 @@
 
-# your solution here 
-def stops_between_stations
-    mbta = {
-        Red:['South Station', 'Park Street', 'Kendall','Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
-        Green: ['Government Center','Park Street', 'Boylston' , 'Arlington' , 'Copley', 'Hynes' , 'Kenmore'],
-        Orang: ['North Station', 'Haymarket', 'Park Street' , 'State' ,'Downtown Crossing' , 'Chinatown' ,'Back Bay' , 'Forest Hills']
-        }
-        
-        def subway (sLine, startS, eLine, endS) {
-            lines = ['Red', 'Green', 'Orange']
-            if (!(sLine && eLine && lines.index(sLine.to_s.downcase >= 0 && lines.index(eLine.to_s.downcase) >= 0))
-                return p "Please select one of available lines: Red, Green, OR Orange"
-        
-            sl = sLine.downcase();
-            el = eLine.downcase();
-        };
-        
-        if (!(startS && endS && subway[sl].index(starS.to_s.downcase) > 0 && stops[el].index(endS.to_s.downcase) > 0))
-                return p "Please select one of the available stations: \n red line stations: South Station, Park Street, Kendall, Central, Harvard, Porter, Davis, and Alewife. \n green line stations: Government Center, Park Street, Boylston, Arlington, Copley, Hynes, and Kenmore. \n orange line stations: North Station, Haymarket, Park Street, State, Downtown Crossing, Chinatown, Back Bay, and Forest Hills."
-        
-            lSStation = starS.downcase();
-            lEStation = endS.downcase();
-        
-            # Finding the postions
-            sS = subway[st].index(lSStation);
-            eS = subway[el].index(lEStation);
-        
-            if (sLine == lELine) {
-                return Math.abs(eS - sS);
-            }
-            end
-        
-            return Math.abs(sS - stops[sLine].index('park street')) + Math.abs(eS - stops[eLine].index('park street'));
-        end
-        
-        }
+# # your solution here 
 
+def stops_between_stations(sLine, sStation, eLine, eStation)
+    lines = {
+        red:[ "South Station","Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"],
+         green:["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"],
+          Orange:["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hill"]
+        }
+    
+    return p (lines[sLine.to_sym].index(sStation) - lines[eLine.to_sym].index(eStation).abs()) if sLine==eLine
+    
+    p ((lines[(sLine.downcase).to_sym].index(sStation) - lines[(sLine.downcase).to_sym].index("Park Street")).abs() ) + ((lines[(eLine.downcase).to_sym].index("Park Street") - lines[(eLine.downcase).to_sym].index(eStation)).abs() )
 end
+   
+    
+    stops_between_stations('red', 'Alewife', 'red', 'Alewife') # 0
+    stops_between_stations('red', 'Alewife', 'red', 'South Station') # 7
+    stops_between_stations('red', 'South Station', 'green', 'Kenmore') # 6 
+    
